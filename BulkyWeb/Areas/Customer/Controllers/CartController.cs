e170101c12c7c1082db7327f3f110677cdf2bdfb
Thanos Tsitsis <athanasios.tsitsis@gmail.com>
2023-05-21 01:06:34 +0100

    Changed domain in controllers

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -121,7 +121,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 				//customer user and need to capture payment
 				//stripe logic
 
-				var domain = "https://localhost:7223/";
+				var domain = Request.Scheme + "://" + Request.Host.Value + "/";
 				var options = new SessionCreateOptions
 				{
 					SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
